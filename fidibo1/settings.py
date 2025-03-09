465af30e4be298aeeea2651a9a10b5f60beea971
Mobinamousavii <mobinamsv6@gmail.com>
2025-03-09 21:11:56 +0330

    urls added for each apps

--- fidibo1/settings.py
+++ fidibo1/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ebooks',
+    'audiobooks',
+    'education',
+    'fidiplus',
+    
 ]
 
 MIDDLEWARE = [
